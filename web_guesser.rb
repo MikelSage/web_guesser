ec55b8f047c3d14ed35e3e48e1a443e882b1f153
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-16 13:19:49 -0600

    add way too iteration

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require "sinatra/reloader"
 
-number = rand(100)
+set :number, rand(1..100)
+
+def check_guess(guess)
+  diff = guess - settings.number
+  if guess.zero?
+    ""
+  elsif guess > settings.number && diff > 5
+    "Way too high!"
+  elsif guess < settings.number && diff < -5
+    "Way too low!"
+  elsif guess > settings.number
+    "Too high!"
+  elsif guess < settings.number
+    "Too low!"
+  else
+    "You got it right!"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess.to_i)
+  erb :index, :locals => {:number => settings.number, :message => message, :guess => guess}
 end
